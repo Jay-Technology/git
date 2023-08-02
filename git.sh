5e07a7f0fc863437e68d92363744c84e613c99c9
Jay goud <jay4cs@gmail.com>
2023-08-02 18:37:36 +0000

    Configurarion Details added

--- git.sh
+++ git.sh
@@ -1,1 +1,11 @@
 git init - initilize empty git Repo
+
+git config --global user.name"jay"--configure name
+git config --global user.email"jpg0389@gmail.com"-configure email
+git config --list -- to show configure details.
+git add  < file name> or . -For all
+git commit -m "Commit message"
+git status -to show the Status of files
+git pull origin main-pull the code
+git push origin main-push the code
+
